2d7a53143ef763ee0722cc11e890ee6bb5e345e5
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-08-17 18:28:13 -0400

    Modificando estructura Banckend

---
+++ server/api/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import morgan from 'morgan'
+
+
+
+//inside your starter code, do this
+//dotenv.config();
+
+const server = express()
+// middleware
+server.use(express.json({ limit: '50mb' })); // para leer traducir el body de tipo POST, ya el next() viene implicito
+server.use(morgan('dev')); // para modo de desarrollo muestar con el parametro dev ruta, mensaje repuesta y lo segundo de ejecucion
+//Seteamos headers para la respuesta que le enviamos al cliente
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); //Autorizo recibir solicitudes de este dominio , qué orígenes pueden acceder a la API.
+  res.header('Access-Control-Allow-Credentials', 'true'); //Autorizo recibir solicitudes que incluyan el encabezado con credenciales
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); //Autorizo recibir solicitudes con dichos hedears
+  res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');//Autorizo las solicitudes tipo GET, POST, OPTIONS, PUT y DELETE.
+  next();
+});
+
+
+
+export default server;
+
---
+++ server/api/src/index.js
@@ -0,0 +1,16 @@
+import server from './app'
+require('dotenv').config();
+
+server.listen(process.env.PORT, () => {
+   console.log('Server listening on Port: ',process.env.PORT ); 
+  });
+
+
+// import { Configuration, OpenAIApi } from "openai";
+// require('dotenv').config()
+
+// const configuration = new Configuration({
+//     organization: "org-oWpxfNprnl36US7Fo6GPfIVu",
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+//console.log(configuration)
