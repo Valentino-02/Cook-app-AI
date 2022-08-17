a59a4ddc3098313fa29bbb8db0fb85072341781c
Valentino <collazo.valentino@gmail.com>
2022-08-17 11:33:09 -0300

    first commit

---
+++ server/api/index.js
@@ -0,0 +1,7 @@
+import { Configuration, OpenAIApi } from "openai";
+require('dotenv').config()
+
+const configuration = new Configuration({
+    organization: "org-oWpxfNprnl36US7Fo6GPfIVu",
+    apiKey: process.env.OPENAI_API_KEY,
+});
